18750352527785d6af3ff954d992521259e0f9aa
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2025-05-04 18:05:56 +0700

    feat: db logger

--- internal/infra/mysql/mysql.go
+++ internal/infra/mysql/mysql.go
@@ -1,10 +1,28 @@
 package mysql
 
 import (
+	"log"
+	"os"
+	"time"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func New(dsn string) (*gorm.DB, error) {
-	return gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold:             time.Second,
+			LogLevel:                  logger.Warn,
+			IgnoreRecordNotFoundError: false,
+			ParameterizedQueries:      true,
+			Colorful:                  true,
+		},
+	)
+
+	return gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
 }
