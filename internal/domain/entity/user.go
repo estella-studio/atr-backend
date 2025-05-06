ec07f27110eb4277ed7f74ba2ee062b2742fd40a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2025-05-06 10:16:15 +0700

    fix: change name and password data type

--- internal/domain/entity/user.go
+++ internal/domain/entity/user.go
@@ -12,8 +12,8 @@ type User struct {
 	ID        uuid.UUID      `json:"id" gorm:"type:char(36);primaryKey"`
 	Email     string         `json:"email" gorm:"type:varchar(128);not null;unique"`
 	Username  string         `json:"username" gorm:"type:varchar(64);not null;unique"`
-	Password  string         `json:"password" gorm:"type:varchar(256);not null"`
-	Name      string         `json:"name" gorm:"type:text"`
+	Password  string         `json:"password" gorm:"type:text;not null"`
+	Name      string         `json:"name" gorm:"type:varchar(128)"`
 	CreatedAt time.Time      `json:"created_at" gorm:"type:timestamp;autoCreateTime"`
 	UpdatedAt time.Time      `json:"updated_at" gorm:"type:timestamp;autoUpdateTime"`
 	DeletedAt gorm.DeletedAt `gorm:"index"`
